0c56a12fc5dc7cba8c602953da84872fa6e494ad
= <=>
2021-04-23 15:43:40 +0700

    RegisterRequest dan controller AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Traits\ApiResponse;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+}
