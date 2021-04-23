0f8bfbd635248d95db301a009ca8fc534820e087
= <=>
2021-04-23 15:52:05 +0700

    menambahkan table todo,TodoController model Todo

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,12 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+
+Route::middleware(['auth:sanctum'])->group (function () {
+    Route::post('/logout',  [AuthController::class, 'logout']);
+}); 
